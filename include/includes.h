f46f56bba9703963bc8e9fc3b1af1eb5274dffc3
WarpMiner <163392665+WarpMiner@users.noreply.github.com>
2024-11-14 20:17:14 +0700

    Create includes.h

---
+++ include/includes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+using namespace std;
+
+#include <set>
+#include <limits>
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <utility>
+#include <queue>
